e8ff042f139c35285eaf60910f8a40ae81e6a968
Tarnished-Arjun <arjunkrishna0081@gmail.com>
2026-03-26 14:39:10 +0530

    Update greater.js

---
+++ greater.js
@@ -0,0 +1,14 @@
+let num1 = Number(prompt("Enter first number:"));
+let num2 = Number(prompt("Enter second number:"));
+let num3 = Number(prompt("Enter third number:"));
+let greatest;
+
+if (num1 >= num2 && num1 >= num3) {
+    greatest = num1;
+} else if (num2 >= num1 && num2 >= num3) {
+    greatest = num2;
+} else {
+    greatest = num3;
+}
+
+alert("The greatest number is: " + greatest);
