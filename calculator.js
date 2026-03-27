3ec10ae4688ac3c63bd39973d69525fc3fb690ae
Tarnished-Arjun <arjunkrishna0081@gmail.com>
2026-03-27 11:34:05 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,30 +1,43 @@
-let num1 = Number(prompt("Enter first number:"));
-let num2 = Number(prompt("Enter second number:"));
-let operator = prompt("Enter operator (+, -, *, /):");
-let result;
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Calculator</title>
+</head>
+<body>
+<script>
+    let num1 = Number(prompt("Enter first number:"));
+    let num2 = Number(prompt("Enter second number:"));
+    let operator = prompt("Enter operator (+, -, *, /):");
 
-switch (operator) {
-    case "+":
-        result = num1 + num2;
-        break;
+    let result;
 
-    case "-":
-        result = num1 - num2;
-        break;
+    switch (operator) {
+        case "+":
+            result = num1 + num2;
+            break;
 
-    case "*":
-        result = num1 * num2;
-        break;
+        case "-":
+            result = num1 - num2;
+            break;
 
-    case "/":
-        if (num2 !== 0) {
-            result = num1 / num2;
-        } else {
-            result = "Cannot divide by zero";
-        }
-        break;
+        case "*":
+            result = num1 * num2;
+            break;
 
-    default:
-        result = "Invalid operaton";
-}
-alert("Result: " + result);
+        case "/":
+            if (num2 !== 0) {
+                result = num1 / num2;
+            } else {
+                result = "Cannot divide by zero!";
+            }
+            break;
+
+        default:
+            result = "Invalid operator!";
+    }
+
+    alert("Result: " + result);
+</script>
+
+</body>
+</html>
