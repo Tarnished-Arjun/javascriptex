1d5b9825c5884097690216b84344a120687a1409
Tarnished-Arjun <arjunkrishna0081@gmail.com>
2026-03-27 12:32:00 +0530

    Update primeornot.js

--- primeornot.js
+++ primeornot.js
@@ -1,17 +1,28 @@
-let num = Number(prompt("Enter a number:"));
-let isPrime = true;
-if (num <= 1) {
-    isPrime = false;
-} else {
-    for (let i = 2; i < num; i++) {
-        if (num % i === 0) {
-            isPrime = false;
-            break;
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Prime Number Check</title>
+</head>
+<body>
+<script>
+    let num = Number(prompt("Enter a number:"));
+    let isPrime = true;
+
+    if (num <= 1) {
+        isPrime = false;
+    } else {
+        for (let i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i === 0) {
+                isPrime = false;
+                break;
+            }
         }
     }
-}
-if (isPrime) {
-    alert(num + " is a Prime Number");
-} else {
-    alert(num + " is NOT a Prime Number");
-}
+    if (isPrime) {
+        alert(num + " is a Prime Number");
+    } else {
+        alert(num + " is NOT a Prime Number");
+    }
+</script>
+</body>
+</html>
