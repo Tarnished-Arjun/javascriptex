4c2cafa9dfac43fd792e755b5fd91387fe1d6e9d
Tarnished-Arjun <arjunkrishna0081@gmail.com>
2026-03-27 12:31:22 +0530

    Update sum.js

--- sum.js
+++ sum.js
@@ -1,7 +1,20 @@
-let n = Number(prompt("Enter a number:"));
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sum of n Natural Numbers</title>
+</head>
+<body>
+<script>
+    let n = Number(prompt("Enter a number:"));
 
-let sum = 0;
-for (let i = 1; i <= n; i++) {
-    sum += i;
-}
-alert("Sum of first " + n + " natural numbers is: " + sum);
+    let sum = 0;
+
+    for (let i = 1; i <= n; i++) {
+        sum += i;
+    }
+
+    alert("Sum of first" + n + " natural numbers is:" + sum);
+</script>
+
+</body>
+</html>
