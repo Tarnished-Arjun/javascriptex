d63ac0a2d7ccb633c37dacc68e8aafdf6d25d3a8
Tarnished-Arjun <arjunkrishna0081@gmail.com>
2026-03-30 13:40:07 +0530

    New assignment

---
+++ Triangle.js
@@ -0,0 +1,5 @@
+function triangleArea(base, height) {
+    return 0.5 * base * height;
+}
+
+console.log(triangleArea(20, 8)); 
---
+++ type.js
@@ -0,0 +1,7 @@
+function findType(value) {
+    return typeof value;
+}
+console.log(findType(10));       
+console.log(findType("hello"));  
+console.log(findType(true));      
+console.log(findType({}));      
