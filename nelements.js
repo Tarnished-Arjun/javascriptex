94e7da3ec012ea36a8b8f718f338606e53a5bf00
Arjun <arjunkrishna0081@gmail.com>
2026-04-01 10:05:48 +0530

    New

---
+++ nelements.js
@@ -0,0 +1,7 @@
+function printfirstN(arr, n) {
+    for (let i = 0; i < n && i < arr.length; i++) {
+        console.log(arr[i]);
+    }
+}
+let nums = [10, 20, 30, 40, 50];
+printfisrtN(nums, 3);
---
+++ sort.js
@@ -0,0 +1,5 @@
+function sortStrings(arr) {
+    return arr.sort();
+}
+let bikes = ["ZX4RR", "Yamaha R1", "Aprillia", "Ducati"];
+console.log(sortStrings(bikes)); 
