041e417dd1380e8b567500b2b4e5511fd5bdde9a
Arjun <arjunkrishna0081@gmail.com>
2026-04-08 17:43:14 +0530

    New

---
+++ es6/callback.js
@@ -0,0 +1,21 @@
+function filterArray(arr, callback) {
+  let result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (callback(arr[i])) {
+      result.push(arr[i]);
+    }
+  }
+
+  return result;
+}
+
+function isEven(num) {
+  return num % 2 === 0;
+}
+
+let numbers = [1, 2, 3, 4, 5, 6];
+
+let evenNumbers = filterArray(numbers, isEven);
+
+console.log(evenNumbers);
